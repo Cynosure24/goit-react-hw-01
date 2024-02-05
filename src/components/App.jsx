2d97cce02b7ea3ec095dbbd5ee421e830c7098a4
Cynosure24 <139391583+Cynosure24@users.noreply.github.com>
2024-02-05 18:18:00 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import { ProfileModule } from './Profile/Profile';
 import userData from './userData.json';
 import { FriendList } from './FriendList/FriendList';
-import friends from '../friends.json';
+import friends from './friends.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import transactions from '../transactions.json';
+import transactions from './transactions.json';
 
 export function App() {
   return (
