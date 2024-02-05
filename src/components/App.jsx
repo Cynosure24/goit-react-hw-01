5ef4902076176ea9b79a7bc6e8b3fba60ef97e14
Cynosure24 <139391583+Cynosure24@users.noreply.github.com>
2024-02-05 16:34:21 +0100

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import { ProfileModule } from './Profile/Profile';
+import userData from '../userData.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../transactions.json';
+
+export function App() {
+  return (
+    <div className="rootMain">
+      <ProfileModule
+        avatar={userData.avatar}
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
