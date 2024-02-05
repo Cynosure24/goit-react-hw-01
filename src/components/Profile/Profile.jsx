929f0654e22e8b019b6ce61dac2a11dc24faa0ae
Cynosure24 <139391583+Cynosure24@users.noreply.github.com>
2024-02-05 18:25:14 +0100

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,34 +1,23 @@
 import css from './Profile.module.css';
 
-export function ProfileModule({
-  username,
-  tag,
-  location,
-  avatar,
-  stats: { followers, views, likes },
-}) {
+export default function Profile({ items: { username, tag, location, avatar, stats } }) {
+  const keys = Object.keys(stats);
   return (
     <div className={css.profile}>
-      <div className={css.description}>
-        <img src={avatar} alt={username} className={css.avatar} />
-        <p className={css.name}>{username}</p>
-        <p className={css.tag}>{tag}@pmarica</p>
-        <p className={css.location}>{location}</p>
-      </div>
+      <img src={avatar} alt="User photo" className={css.avatar} />
+      <p className={css.name}>{username}</p>
+      <p className={css.tag}>@{tag}</p>
+      <p className={css.location}>{location}</p>
 
       <ul className={css.stats}>
-        <li className={css.statsItem}>
-          <span className={css.label}>Followers</span>
-          <span className={css.value}>{followers}</span>
-        </li>
-        <li className={css.statsItem}>
-          <span className={css.label}>Views</span>
-          <span className={css.value}>{views}</span>
-        </li>
-        <li className={css.statsItem}>
-          <span className={css.label}>Likes</span>
-          <span className={css.value}>{likes}</span>
-        </li>
+        {keys.map(item => {
+          return (
+            <li className={css.statsItem} key={item}>
+              <span className={css.label}>{item}</span>
+              <span className={css.statsData}>{stats[item]}</span>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
